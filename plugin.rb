abcc5586998f6fe0417cd61f694f9dfc683ab80f
Jay Pfaffman <jay@literatecomputing.com>
2024-01-09 09:23:23 -0600

    fix paths, hide from anon

--- plugin.rb
+++ plugin.rb
@@ -1,24 +1,24 @@
 # frozen_string_literal: true
 
-# name: display-email
+# name: discourse-display-email
 # about: Display email address on user card and profile page
 # meta_topic_id: TODO
 # version: 0.0.1
 # authors: pfaffman
-# url: https://github.com/literatecomputing/display-email
+# url: https://github.com/literatecomputing/discourse-display-email
 # required_version: 2.7.0
 
 enabled_site_setting :display_email_enabled
 
 module ::DisplayEmailModule
-  PLUGIN_NAME = "display-email"
+  PLUGIN_NAME = "discourse-display-email"
 end
 
-require_relative "lib/display-email/engine"
+require_relative "lib/discourse_display_email/engine"
 
 after_initialize do
   add_to_serializer(:user_card, :email) do
     puts "selfserial #{self}"
-    self.user.email 
+    scope.anonymous? ? nil : self.user.email 
   end
 end
